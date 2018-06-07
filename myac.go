2c69c5faa3fe148ee96db7f92722559dba54dbfa
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-06-07 18:34:26 +0300

    use config.yml from host

--- myac.go
+++ myac.go
@@ -1,1 +1,34 @@
-package myac
+package main
+
+import(
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"fmt"
+)
+
+type conf struct {
+	Foo string
+	Bar []string
+}
+
+func (c *conf) getConf() *conf {
+	yamlFile, err := ioutil.ReadFile("config.yml")
+
+	if err != nil {
+		log.Printf("yamlFile.Get err #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c
+}
+
+func main() {
+	var c conf
+	c.getConf()
+
+	fmt.Println(c)
+}
