c7ec0e2e63aa5069cbfc7f3f5b025458d58fac11
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-06-12 17:27:28 +0300

    add property for server port and repository local path

--- myac.go
+++ myac.go
@@ -7,9 +7,9 @@ import(
 	"fmt"
 	"gopkg.in/src-d/go-git.v4"
 	"os"
-	"gopkg.in/src-d/go-git.v4/storage/memory"
 	"github.com/kataras/iris"
 	"path/filepath"
+	"strings"
 )
 
 type ServerConf struct {
@@ -46,12 +46,7 @@ func main() {
 	var c ServerConf
 	var url string = c.getConf().Server.Git.URI
 	fmt.Println(url)
-	_, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions {
-		URL: url,
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
+
 
 	_, err1 := git.PlainClone(".filesystem-repo", false, &git.CloneOptions{
 		URL: url,
@@ -61,15 +56,20 @@ func main() {
 		log.Println(err1)
 	}
 	repo, err := listRepo(".filesystem-repo")
+	if err != nil {
+		log.Println(err)
+	}
 	fmt.Println(repo)
 	runServer()
 }
 
-func listRepo(pathToRepo string) ([]string, error) {
+func listRepo(root string) ([]string, error) {
 	var files []string
-	err := filepath.Walk(pathToRepo, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			files = append(files, path)
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() && !strings.HasPrefix(path, ".filesystem-repo/.git"){
+			fmt.Println(path)
+			serviceURI := strings.TrimPrefix(path, ".filesystem-repo/")
+			files = append(files, serviceURI)
 		}
 		return nil
 	})
