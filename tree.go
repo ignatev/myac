39acc1fdc5263ca4640d5e31e91be0fbc83c5ef2
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-07-27 21:17:26 +0300

    draft dir walk method

--- tree.go
+++ tree.go
@@ -1,5 +1,24 @@
 package main
 
-func tree(path string) string {
-	return path
+import (
+	"path/filepath"
+	"os"
+)
+
+//└ ─ │ ├
+const (
+	newLine			= "\n"
+	emptySpace  	= "    "
+	middleItem		= "├── "
+	continueItem	= "│   "
+	lastItem 		= "└── "
+)
+
+func tree(dir string) string {
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			err.Errorf("Fail to access a path %q: %v\n", dir, err)
+		}
+	})
+
 }
