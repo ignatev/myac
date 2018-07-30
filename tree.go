4ef5197a110fe992a9b07b65991ab6d11b8eafbd
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-07-30 21:13:01 +0300

    use ioutil.ReadDir() instead of filepath.Walk()

--- tree.go
+++ tree.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-//	"io/ioutil"
-//	"log"
-	"os"
-	"path/filepath"
+	"io/ioutil"
+	"log"
+//	"os"
+//	"path/filepath"
 )
 
 //└ ─ │ ├
@@ -25,25 +25,39 @@ type configDirectory struct {
 }
 
 func tree(dir string) (string, error) {
+	
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	var cds []configDirectory
-	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
-		var filePaths []string
-		var dirPaths []string
-		if f.IsDir() {
-			dirPaths = append(dirPaths, f.Name())
-			fmt.Println(dirPaths)
-		}
-		if !f.IsDir() {
-			filePaths = append(filePaths, f.Name())
-			fmt.Println(filePaths)
-		}
-		cd := configDirectory{dir, "", filePaths, dirPaths}
-		fmt.Println(cd)
-		cds = append(cds, cd)
+	for _, file := range files {
+		fmt.Println(file.Name())
+		fmt.Println(file.IsDir())
+	}
+	return "", nil
 
-		return nil
-	})
-	fmt.Println(cds)
-	return "", err
+	/*
+		var cds []configDirectory
+		err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+			var filePaths []string
+			var dirPaths []string
+			if f.IsDir() {
+				dirPaths = append(dirPaths, f.Name())
+				fmt.Println(dirPaths)
+			}
+			if !f.IsDir() {
+				filePaths = append(filePaths, f.Name())
+				fmt.Println(filePaths)
+			}
+			cd := configDirectory{dir, "", filePaths, dirPaths}
+			fmt.Println(cd)
+			cds = append(cds, cd)
+
+			return nil
+		})
+		fmt.Println(cds)
+		return "", err
+	*/
+
 }
